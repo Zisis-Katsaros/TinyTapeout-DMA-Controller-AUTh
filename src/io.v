89454cf0c05202553e42c0349f203c8d5292341c
Zafeiris <zafeiris@example.com>
2026-04-05 03:57:12 +0300

    Created the top module and the memory.v and io.v for testing. TODO: Change project.v to include SRC_ack and DEST_ack in the ui_in (make the cfg_in smaller --> 2 bits) and testing

---
+++ src/io.v
@@ -0,0 +1,376 @@
+`default_nettype none
+
+module io (output reg fetch, output reg IO_ack, inout wire [7:0] data, input [6:0] ins, input clk, input rst_n);
+
+    wire BG, done, valid, WRITE_en, mode, direction, DMA_ack;
+
+    assign valid = ins[0];
+    assign WRITE_en = ins[1];
+    assign done = ins[2];
+    assign direction = ins[3];
+    assign DMA_ack = ins[4];
+    assign mode = ins[5];
+    assign BG = ins[6];
+
+    // -------- HANDLING THE BIDIRECTIONAL PORT -----------------
+
+    reg [7:0] data_out;
+    reg data_oe;   // output enable
+    wire [7:0] data_in;
+
+    assign data = data_oe ? data_out : 8'bz; // drive or high-Z
+    assign data_in = data;                   // read
+
+    // ---------- END -----------------
+
+    // ----------- SYNCHRONISERS ------------------
+
+  reg valid_sync2, valid_sync1;
+
+  always @(posedge clk or negedge rst_n)
+  begin
+
+    if (!rst_n)
+    begin
+      valid_sync1 <= 1'b0; // check this reset logic
+      valid_sync2 <= 1'b0;
+    end 
+    else
+    begin
+      valid_sync1 <= valid;
+      valid_sync2 <= valid_sync1;
+    end
+
+  end
+
+  reg BG_sync2, BG_sync1;
+
+  always @(posedge clk or negedge rst_n)
+  begin
+
+    if (!rst_n)
+    begin
+      BG_sync1 <= 1'b0; // check this reset logic
+      BG_sync2 <= 1'b0;
+    end 
+    else
+    begin
+      BG_sync1 <= BG;
+      BG_sync2 <= BG_sync1;
+    end
+
+  end
+
+  reg done_sync2, done_sync1;
+
+  always @(posedge clk or negedge rst_n)
+  begin
+
+    if (!rst_n)
+    begin
+      done_sync1 <= 1'b0; // check this reset logic
+      done_sync2 <= 1'b0;
+    end 
+    else
+    begin
+      done_sync1 <= done;
+      done_sync2 <= done_sync1;
+    end
+
+  end
+
+  reg WRITE_en_sync2, WRITE_en_sync1;
+
+  always @(posedge clk or negedge rst_n)
+  begin
+
+    if (!rst_n)
+    begin
+      WRITE_en_sync1 <= 1'b0; // check this reset logic
+      WRITE_en_sync2 <= 1'b0;
+    end 
+    else
+    begin
+      WRITE_en_sync1 <= WRITE_en;
+      WRITE_en_sync2 <= WRITE_en_sync1;
+    end
+
+  end
+
+  reg mode_sync2, mode_sync1;
+
+  always @(posedge clk or negedge rst_n)
+  begin
+
+    if (!rst_n)
+    begin
+      mode_sync1 <= 1'b0; // check this reset logic
+      mode_sync2 <= 1'b0;
+    end 
+    else
+    begin
+      mode_sync1 <= mode;
+      mode_sync2 <= mode_sync1;
+    end
+
+  end
+
+  reg direction_sync2, direction_sync1;
+
+  always @(posedge clk or negedge rst_n)
+  begin
+
+    if (!rst_n)
+    begin
+      direction_sync1 <= 1'b0; // check this reset logic
+      direction_sync2 <= 1'b0;
+    end 
+    else
+    begin
+      direction_sync1 <= direction;
+      direction_sync2 <= direction_sync1;
+    end
+
+  end
+
+  reg DMA_ack_sync2, DMA_ack_sync1;
+
+  always @(posedge clk or negedge rst_n)
+  begin
+
+    if (!rst_n)
+    begin
+      DMA_ack_sync1 <= 1'b0; // check this reset logic
+      DMA_ack_sync2 <= 1'b0;
+    end 
+    else
+    begin
+      DMA_ack_sync1 <= DMA_ack;
+      DMA_ack_sync2 <= DMA_ack_sync1;
+    end
+
+  end
+
+//------------ END of synchroniserss --------------
+
+    // memory regfile 
+
+  reg [7:0] regfile_write_data, regfile_read_data, regfile_address;
+  reg [7:0] regs [255:0];
+
+  integer i;
+  reg write;
+
+  always @(posedge clk or negedge rst_n)
+  begin
+
+    if (!rst_n)
+    begin
+
+      for (i=0; i<256; i++)
+        regs[i] = 8'b0;     // error for <=
+
+    end
+    else if (write)
+    begin
+
+      regs[regfile_address] <= regfile_write_data;
+
+    end
+    else
+    begin
+
+      regfile_read_data <= regs[regfile_address];
+
+    end
+
+  end
+
+    // Destination address and data regfile
+
+  reg [7:0] write_target_address, read_target_address, target_address;
+  reg write2;
+
+  always @(posedge clk or negedge rst_n)
+  begin
+
+    if (!rst_n)
+    begin
+
+      for (i=0; i<1; i++)
+        target_address <= 8'b0;
+
+    end
+    else if (write2)
+    begin
+
+      target_address <= write_target_address;
+
+    end
+    else
+    begin
+
+      read_target_address <= target_address;
+
+    end
+
+  end
+
+// ------  COUNTER ---------
+
+  reg [4:0] cnt;
+
+  always @(posedge clk or negedge rst_n)
+  begin
+
+    if (!rst_n)
+      cnt <= 0;
+    else if (current_state == SOURCE2)
+      cnt <= cnt + 1'b1;
+    else
+      cnt <= 0;
+
+  end
+
+    // ---------- FSM -----------------
+
+    localparam WAITING = 4'b000;
+    localparam ROLE = 4'b001;
+    localparam SOURCE1 = 4'b010;
+    localparam SOURCE2 = 4'b011;
+    localparam DESTINATION1 = 4'b100;
+    localparam DESTINATION2 = 4'b101;
+    localparam DONE_STATE = 4'b110;
+    localparam ACKNOWLEDGMENT = 4'b111;
+    localparam DEST2_ACKNOWLEDGMENT = 4'b1000;
+
+    reg [3:0] current_state, next_state;
+
+    always @(posedge clk or negedge rst_n)
+    begin
+
+        if (!rst_n)
+            current_state <= WAITING;
+        else
+            current_state <= next_state;
+
+    end
+
+    always @(current_state or BG or valid_sync2 or direction_sync2 or mode_sync2 or done_sync2 or WRITE_en_sync2)
+    begin
+
+        case (current_state)
+        
+            WAITING     : if (BG_sync2) next_state = ROLE; else next_state = WAITING; // if we took BG we took mode and direction as well, it is sent last
+
+            ROLE        : if (!direction_sync2) next_state = SOURCE1; else if (direction_sync2) next_state = DESTINATION1; else next_state = ROLE;
+
+            SOURCE1     : if (valid_sync2 && !WRITE_en_sync2) next_state = ACKNOWLEDGMENT; else next_state = SOURCE1;
+
+            ACKNOWLEDGMENT : if (!valid_sync2 && !direction_sync2) next_state = SOURCE2; else if (!valid_sync2 && direction_sync2) next_state = DESTINATION2; else next_state = ACKNOWLEDGMENT;
+
+            DEST2_ACKNOWLEDGMENT : if (!valid_sync2) next_state = DONE_STATE; else next_state = DEST2_ACKNOWLEDGMENT;
+
+            SOURCE2     : if (DMA_ack_sync2) next_state = DONE_STATE; else next_state = SOURCE2;
+
+            DESTINATION1: if (valid_sync2 && WRITE_en_sync2) next_state = ACKNOWLEDGMENT; else next_state = DESTINATION1;
+
+            DESTINATION2: if (valid_sync2 && WRITE_en_sync2) next_state = DEST2_ACKNOWLEDGMENT; else next_state = DESTINATION2;
+
+            DONE_STATE  : if (mode_sync2) next_state = SOURCE1; else if (done_sync2) next_state = WAITING; else next_state = DONE_STATE;
+
+            default     : next_state = WAITING;
+
+        endcase
+
+    end
+
+    always @(current_state or WRITE_en_sync2 or valid_sync2)
+    begin
+
+        regfile_address = 0;
+        write = 0;
+        write2 = 0;
+        IO_ack = 0;
+        fetch = 0;
+        write_target_address = 0;
+        regfile_write_data = 0;
+        data_oe = 0;
+
+        case (current_state)
+
+            WAITING     : IO_ack = 1'bz;
+
+            ROLE        : IO_ack = 1'bz;
+
+            SOURCE1     : 
+            begin
+
+                if (valid_sync2 && !WRITE_en_sync2)
+                begin
+
+                    write_target_address = data_in; // we will have the data in
+                    write2 = 1; 
+                    IO_ack = 1; // make this source acknowledgment
+
+                end
+
+            end
+
+            SOURCE2     :
+            begin
+
+                // Send data from source address to DMA
+
+                if (cnt == 0)
+                    regfile_address = read_target_address;
+                else
+                begin
+                    data_oe = 1;
+                    data_out = regfile_read_data;
+                    fetch = 1;
+                end
+
+            end
+
+            DESTINATION1:
+            begin
+
+                if (valid_sync2 && WRITE_en_sync2)
+                begin
+
+                    write_target_address = data_in;
+                    write2 = 1;
+                    IO_ack = 1; // must become source_ack
+
+                end
+
+            end
+
+            ACKNOWLEDGMENT  : IO_ack = 1; // Sending ack until we are sure the DMA continues
+
+            DEST2_ACKNOWLEDGMENT    : IO_ack = 1;
+
+            DESTINATION2:
+            begin
+
+                if (valid_sync2 && WRITE_en_sync2)
+                begin
+                    
+                    regfile_write_data = data_in;
+                    regfile_address = target_address;
+                    write = 1;
+                    IO_ack = 1; // must become source_ack
+
+                end
+
+            end
+
+            default     :  data = 0;
+
+        endcase
+
+    end
+
+
+endmodule
--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_example_zafeiris (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,11 +17,440 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  // assign uio_out = 0;
+  // assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, 1'b0};
+
+  // -------------- DECLARING ACKNOWLEDGMENT VALUES STORED IN ui_i[0] and ui_i[1] (reused) ---------------
+
+  wire SRC_ack, DEST_ack;
+
+  assign SRC_ack = ui_in[0];
+  assign DEST_ack = ui_in[1]; 
+
+  // --------------     END     -----------------------
+
+  // Synchronizers
+
+  reg SRC_ack_sync1, SRC_ack_sync2;
+
+  always @(posedge clk or negedge rst_n)
+  begin
+
+    if (!rst_n)
+    begin
+      SRC_ack_sync1 <= 1'b0; // check this reset logic
+      SRC_ack_sync2 <= 1'b0;
+    end
+    else
+    begin
+      SRC_ack_sync1 <= SRC_ack;
+      SRC_ack_sync2 <= SRC_ack_sync1;
+    end
+  end
+
+  reg DEST_ack_sync1, DEST_ack_sync2;
+
+  always @(posedge clk or negedge rst_n)
+  begin
+
+    if (!rst_n)
+    begin
+      DEST_ack_sync1 <= 1'b0; // check this reset logic
+      DEST_ack_sync2 <= 1'b0;
+    end
+    else
+    begin
+      DEST_ack_sync1 <= DEST_ack;
+      DEST_ack_sync2 <= DEST_ack_sync1;
+    end
+  end
+
+  reg fetch_sync2, fetch_sync1;
+
+  always @(posedge clk or negedge rst_n)
+  begin
+
+    if (!rst_n)
+    begin
+      fetch_sync1 <= 1'b0; // check this reset logic
+      fetch_sync2 <= 1'b0;
+    end
+    else
+    begin
+      fetch_sync1 <= fetch;
+      fetch_sync2 <= fetch_sync1;
+    end
+
+
+  end
+
+  always @(posedge clk or negedge rst_n)
+  begin
+
+    if (!rst_n)
+      cnt <= 0;
+    else if ((current_state == IDLE_PREPARATION && enable == 1) || (current_state == DELAY))
+      cnt <= cnt + 1'b1;
+    else
+      cnt <= 0;
+
+  end
+
+  // Reg_file to hold source_addr and dest_addr
+
+  reg [3:0] regs_for_addr [3:0];
+  reg regs [1:0];
+
+  reg [7:0] source_addr, dest_addr;
+
+  reg [3:0] b4_config_address;
+  reg data;
+  reg [7:0] data2;
+
+  reg [1:0] address1;
+  reg address2;
+
+  reg write, write2, write3; // Using this to change the regs data only when we want, else they are left with the last value
+
+  reg [7:0] source_data;
+  reg [7:0] regs_source_data;
+
+  integer i;
+
+  // regfile for addresses
+
+  always @(posedge clk or negedge rst_n)
+  begin
+
+    if (!rst_n)
+    begin
+
+      for (i=0; i<4; i++)
+        regs_for_addr[i] <= 4'b0;
+
+    end
+    else if (write)
+    begin
+
+      regs_for_addr[address1] <= b4_config_address;
+
+    end
+    else
+    begin
+
+      source_addr <= {regs_for_addr[1], regs_for_addr[0]};
+      dest_addr <= {regs_for_addr[3], regs_for_addr[2]};
+
+    end
+
+  end
+
+  // regfile for direction, BG and mode
+
+  always @(posedge clk or negedge rst_n)
+  begin
+
+    if (!rst_n)
+    begin
+
+      for (i=0; i<2; i++)
+        regs[i] <= 1'b0;
+
+    end
+    else if (write2)
+    begin
+
+      regs[address2] <= data; // address2 = 0 mode, 1 direction
+
+    end
+    else
+    begin
+
+      mode <= regs[0];
+      direction <= regs[1];
+
+    end
+
+  end
+
+  // regfile for source_data
+
+  always @(posedge clk or negedge rst_n)
+  begin
+
+    if (!rst_n)
+    begin
+
+      regs_source_data <= 8'd0;
+
+    end
+    else if (write3)
+    begin
+
+      regs_source_data <= data2; // address2 = 0 mode, 1 direction, 2 source_data
+
+    end
+    else
+    begin
+
+      source_data <= regs_source_data;
+
+    end
+
+  end
+
+  // Input registers 
+
+  reg mode; // 0 single - 1 burst
+  reg direction; // 0 memory --> io, 1 io --> memory
+  wire fetch; // fetch/acknowledgement
+  wire BG;
+  wire enable;
+
+  assign enable = ui_in[7];
+  assign BG = ui_in[6];
+  assign fetch = ui_in[5];
+
+  // Output registers
+
+  reg valid;
+  reg done;
+  reg WRITE_en;
+  reg BR;
+  // -- done with mode -- reg bus_dir; // same values as direction --> informs memory and io who is source and who destination (will be used in testbench)
+  reg ack;
+
+  // Output assignments
+
+  assign uo_out[0] = valid;
+  assign uo_out[1] = WRITE_en;
+  assign uo_out[2] = done;
+  assign uo_out[3] = direction; // bus_dir
+  assign uo_out[4] = ack;
+  assign uo_out[5] = mode;
+  assign uo_out[6] = BR;
+
+
+  // Data BUS
+
+  reg [7:0] transfer_bus;
+  assign uio_out = transfer_bus;
+
+  // FSM 
+
+  reg [3:0] current_state, next_state;
+
+  reg [6:0] cnt;
+
+  localparam IDLE_PREPARATION = 4'b000;
+  localparam BUS_REQ = 4'b001;
+  localparam DMA_to_SRC = 4'b010;
+  localparam SRC_to_DMA = 4'b011;
+  localparam DMA_to_DEST_addr = 4'b100;
+  localparam DELAY = 4'b101;
+  localparam DMA_to_DEST_data = 4'b110;
+  localparam DONE = 4'b111;
+  localparam ACKNOWLEDGMENT = 4'b1000;
+
+  // Bidirectional bus
+
+  assign uio_oe = ((current_state == DMA_to_SRC) || (current_state == DMA_to_DEST_data) || (current_state == DMA_to_DEST_addr)) ? 8'b1111_1111 : 8'b0000_0000; 
+
+  // FSM implementation
+
+  always @(posedge clk or negedge rst_n) 
+  begin
+
+    if (!rst_n)
+      current_state <= IDLE_PREPARATION;
+    else
+      current_state <= next_state;
+    
+  end
+
+  always @(current_state or enable or BG or fetch or cnt)
+  begin
+
+    case (current_state)
+      
+      IDLE_PREPARATION: if (enable && cnt == 3) next_state = BUS_REQ; else next_state = IDLE_PREPARATION;
+
+      BUS_REQ         : if (BG) next_state = DMA_to_SRC; else next_state = BUS_REQ;
+
+      DMA_to_SRC      : if (SRC_ack_sync2) next_state = SRC_to_DMA; else next_state = DMA_to_SRC; // ui_in[0] --> after loading data used for synchronized source acknowledgment
+
+      SRC_to_DMA      : if (fetch_sync2) next_state = ACKNOWLEDGMENT; else next_state = SRC_to_DMA;
+
+      DMA_to_DEST_addr: if (DEST_ack_sync2) next_state = DELAY; else next_state = DMA_to_DEST_addr;
+
+      DELAY           : if (cnt == 100) next_state = DMA_to_DEST_data; else next_state = DELAY;
+
+      DMA_to_DEST_data: if (DEST_ack_sync2) next_state = DONE; else next_state = DMA_to_DEST_data;
+      
+      ACKNOWLEDGMENT : if (!fetch_sync2) next_state = DMA_to_DEST_addr; else next_state = ACKNOWLEDGMENT; 
+
+      DONE            : next_state = IDLE_PREPARATION;
+
+      default         : next_state = IDLE_PREPARATION;
+
+
+    endcase
+
+
+  end
+
+  always @(current_state or enable or BG or fetch_sync2 or cnt)
+  begin
+
+    // Defaults
+
+    write = 0;
+    write2 = 0;
+    write3 = 0;
+    address1 = 0;
+    address2 = 0;
+    b4_config_address = 0;
+    data = 0;
+    data2 = 0;
+    BR = 1'b0;
+    valid = 1'd0;
+    transfer_bus = 8'd0;
+    WRITE_en = 0;
+    ack = 0;
+    done = 0;
+
+    case (current_state)
+
+      IDLE_PREPARATION      : //1st state
+      begin
+
+        if (enable)
+        begin
+
+          if (cnt == 0)
+          begin
+
+            // Indicate writing
+            write = 1;
+            write2 = 1;
+
+            // Store the data in the proper regfile addresses 
+            address1 = 2'd0; // first 4 bits of source address
+            address2 = 1'd0; 
+
+            // Get the input data
+            data = ui_in[4]; // get mode
+            b4_config_address = ui_in[3:0];
+          
+          end
+
+          else if (cnt == 1)
+          begin
+
+            write = 1;
+            write2 = 1;
+
+            // Store the data in the proper regfile addresses 
+            address1 = 2'd1; // second 4 bits of source address
+            address2 = 1'd1; 
+
+            // Get the input data
+            data = ui_in[4]; // get direction
+            b4_config_address = ui_in[3:0];            
+
+          end
+
+          else if (cnt == 2)
+          begin
+
+            write = 1;
+            write2 = 0;
+
+            // Store the data in the proper regfile addresses 
+            address1 = 2'd2; // first 4 bits of dest address
+
+            // Get the input data
+            b4_config_address = ui_in[3:0];
+
+          end
+
+          else if (cnt == 3)
+          begin
+
+            write = 1;
+            write2 = 0;
+
+            // Store the data in the proper regfile addresses 
+            address1 = 2'd3; // second 4 bits of dest address
+
+            // Get the input data
+            b4_config_address = ui_in[3:0];
+
+          end
+      
+        end
+
+      end
+
+      BUS_REQ               : BR = 1'b1;  // 2nd state
+
+      DMA_to_SRC            :
+      begin
+
+        transfer_bus = source_addr;
+        valid = 1'd1;
+        WRITE_en = 1'd0;
+
+      end
+
+      SRC_to_DMA            :
+      begin
+
+        if (fetch_sync2)
+        begin
+
+          data2 = uio_in; // Storing data in source_data
+          write3 = 1;
+          ack = 1;
+
+        end
+          
+
+      end
+
+      DMA_to_DEST_addr      :
+      begin
+
+        transfer_bus = dest_addr;
+        valid = 1'd1;
+        WRITE_en = 1'd1;
+
+      end
+
+      DELAY                : valid = 0; // Sending for a bit more time that valid went low to indicate that the acknowledgment was successsfull
+
+      ACKNOWLEDGMENT       : ack = 1; // This state ensures that we will inform that we got the data
+
+      DMA_to_DEST_data     :
+      begin
+
+        transfer_bus = source_data;
+        valid = 1'd1;
+        WRITE_en = 1'd1;
+
+      end
+
+      DONE                  : done = 1'd1;
+
+      default               : done = 0;
+
+    endcase
+    
+
+  end
 
 endmodule
